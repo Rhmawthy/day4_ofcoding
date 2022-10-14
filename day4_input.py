63433913ae8f1d9243e0f1440283015544218cae
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-14 20:31:44 +0800

    Add files via upload

---
+++ day4_input.py
@@ -0,0 +1,7 @@
+#input
+#dimana si user yang memasukkan nilai pada variabel
+#input ini membaca dari keyboard sebagai string, baik itu tertutup dengan tanda kutip 2 ("") ataupun tidak
+#contoh penggunaan input
+name = input ("enter your name:")
+print( "hello",name,"How are you today")
+
